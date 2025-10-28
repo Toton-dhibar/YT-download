b1adfc23c021513c43d9c188f6a1e85f3a92bb31
TOTON DHIBAR <127105435+Toton-dhibar@users.noreply.github.com>
2025-10-28 14:16:06 +0530

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,1 +1,21 @@
+import { createProxyServer } from 'http-proxy';
 
+const proxy = createProxyServer({
+  target: 'http://20.192.29.205:80', // your real V2Ray server
+  changeOrigin: true,
+  ws: true,
+  secure: false
+});
+
+export default function handler(req, res) {
+  proxy.web(req, res, {}, err => {
+    res.statusCode = 500;
+    res.end('Proxy error: ' + err.message);
+  });
+}
+
+export const config = {
+  api: {
+    bodyParser: false
+  }
+};
